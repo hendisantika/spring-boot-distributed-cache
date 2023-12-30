a36d785cbe7060e1068c40d48fbb99735619eee6
hendisantika <hendisantika@yahoo.co.id>
2023-12-31 06:44:31 +0700

    Add RedisProperties 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/distributedcache/config/RedisProperties.java
@@ -0,0 +1,35 @@
+package com.hendisantika.distributedcache.config;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.Duration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/31/23
+ * Time: 06:44
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Validated
+@SuppressWarnings("NullAway.Init")
+@ConfigurationProperties(prefix = "cache.redis")
+public class RedisProperties {
+    @NotNull
+    private Duration ttl;
+
+    @NotNull
+    @NestedConfigurationProperty
+    private RedisConfig config;
+
+    @NotNull
+    private CompressionAlgorithm compressionAlgorithm;
+}

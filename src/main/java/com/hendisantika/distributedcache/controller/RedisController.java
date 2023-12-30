b95b3c4c3f64ad809aa228ee2fdff5001850d9a9
hendisantika <hendisantika@yahoo.co.id>
2023-12-31 06:55:33 +0700

    Add getItem in RedisController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/distributedcache/controller/RedisController.java
+++ src/main/java/com/hendisantika/distributedcache/controller/RedisController.java
@@ -2,9 +2,15 @@ package com.hendisantika.distributedcache.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.distributedcache.config.RedisProperties;
+import com.hendisantika.distributedcache.model.FallbackDTO;
+import com.hendisantika.distributedcache.model.ItemDTO;
+import com.hendisantika.distributedcache.model.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +33,18 @@ public class RedisController {
     private final ObjectMapper objectMapper;
     private final RedisProperties redisProperties;
 
+    @GetMapping(path = "/{itemId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ItemType getItem(@PathVariable("itemId") String itemId) {
+        try {
+            byte[] cachedContent = redisTemplate.opsForValue().get(itemId);
+            if (cachedContent != null) {
+                return objectMapper.readValue(cachedContent, ItemDTO.class);
+            }
+        } catch (Exception e) {
+            log.error("Error while getting item from cache", e);
+        }
+        // Fetch Data from the Database
+        // Write Data into the Cache
+        return new FallbackDTO("Cache miss;");
+    }
 }
